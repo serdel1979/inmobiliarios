3695b58c36f4754c63ec342f6b9e3f8c05e37c10
unknown <sdlbsso@gmail.com>
2022-04-07 12:51:31 -0300

    servicios

---
+++ src/app/services/inmobiliarios.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inmobiliario } from 'src/assets/data/inmobiliarios';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InmobiliariosService {
+
+  constructor(private http: HttpClient) { }
+
+  AllInmobiliarios: Inmobiliario[] = [];
+
+
+  getInmobiliarios(): Observable<Inmobiliario[]>{
+    return this.http.get<Inmobiliario[]>('http://localhost:3000/inmobiliarios');
+  }
+
+  
+  agregaInmobiliario(inmobiliario: Inmobiliario){
+    return this.http.post<Inmobiliario>('http://localhost:3000/inmobiliarios', inmobiliario);
+  }
+
+
+  getInmobiliario(id:string): Observable<Inmobiliario>{
+    return this.http.get<Inmobiliario>(`http://localhost:3000/inmobiliarios/${id}`);
+  }
+
+
+
+}
---
+++ src/assets/data/inmobiliarios.ts
@@ -0,0 +1,13 @@
+export interface Inmobiliario {
+    nombre: string;
+    tipo: string;
+    titulares: number;
+    estado: string;
+    idCou: string;
+    idMae: string;
+    subestado: string;
+    expediente: string;
+    acta: string;
+    num_admin: string;
+    fecha: Date;
+  }
