678cfdb17209b2e73bd0f5af0008269d48c5a704
Sergio De Luca <sdlbsso@gmail.com>
2022-05-29 02:48:36 -0300

    haciendo editar

--- src/app/components/dashboard/inmobiliarios/editar-inmobiliario/editar-inmobiliario.component.ts
+++ src/app/components/dashboard/inmobiliarios/editar-inmobiliario/editar-inmobiliario.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { District } from 'src/app/interfaces/distritos';
 import { Inmobiliario } from 'src/app/interfaces/inmobiliarios';
+import { TypeRealState } from 'src/app/interfaces/type_real_state';
 import { InmobiliariosService } from 'src/app/services/inmobiliarios.service';
 
 @Component({
@@ -11,24 +13,11 @@ import { InmobiliariosService } from 'src/app/services/inmobiliarios.service';
 })
 export class EditarInmobiliarioComponent implements OnInit {
 
-  form!: FormGroup;
+  public form!: FormGroup;
+  types : TypeRealState[] = [];
+  districts : District[] = [];
 
-
-  calificaciones = [
-    { value: 'Club de campo', viewValue: 'Club de campo' },
-    { value: 'Barrio cerrado', viewValue: 'Barrio cerrado' },
-    { value: 'Club de chacra', viewValue: 'Club de chacra' },
-    { value: 'Urbano', viewValue: 'Urbano' },
-  ];
-
-
-  estado = [
-    { value: 'Asignación de valores', viewValue: 'Asignación de valores' },
-    { value: 'Acto administrativo', viewValue: 'Acto administrativo' },
-    { value: 'Análisis', viewValue: 'Análisis' },
-    { value: 'Seguimiento', viewValue: 'Seguimiento' },
-    { value: 'Descartado', viewValue: 'Descartado' }
-  ]
+  
 
   inmobiliario!: Inmobiliario;
   id!: string;
@@ -38,16 +27,25 @@ export class EditarInmobiliarioComponent implements OnInit {
    
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+
+
+    await this.service.getTypeRealState().subscribe(data=>{
+      this.types = data;
+    });
+    await this.service.getDistricts().subscribe(data=>{
+      this.districts = data;
+    })
+
+
     this.id = this.rutaActiva.snapshot.params['id'];
     this.form = this.fb.group({
       name: ['', Validators.required],
-      type: ['', Validators.required],
-      id_cou: ['', Validators.required],
+      type_real_state: ['', Validators.required],
+      id_cou: [''],
       priority: [''],
       register_source: [''],
       current_state: [''],
-      type_real_state: [''],
       district: [''],
       id_mae: [''],
       holders_count: [''],
@@ -56,12 +54,11 @@ export class EditarInmobiliarioComponent implements OnInit {
     this.service.getInmobiliario(this.id).subscribe((data:Inmobiliario) => {
       this.inmobiliario=data;
       this.form.controls['name'].setValue(this.inmobiliario['name']);
-      this.form.controls['type'].setValue(this.inmobiliario['type']);
+      this.form.controls['type_real_state'].setValue(this.inmobiliario['type_real_state']);
       this.form.controls['id_cou'].setValue(this.inmobiliario['id_cou']);
       this.form.controls['priority'].setValue(this.inmobiliario['priority']);
       this.form.controls['register_source'].setValue(this.inmobiliario['register_source']);
       this.form.controls['current_state'].setValue(this.inmobiliario['current_state']);
-      this.form.controls['type_real_state'].setValue(this.inmobiliario['type_real_state']);
       this.form.controls['district'].setValue(this.inmobiliario['district']);
       this.form.controls['id_mae'].setValue(this.inmobiliario['id_mae']);
       this.form.controls['holders_count'].setValue(this.inmobiliario['holders_count']);
